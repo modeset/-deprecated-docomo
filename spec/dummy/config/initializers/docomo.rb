3a62e3bc7a090ce9e808f42a9b96d4a462659a35
jejacks0n <jejacks0n@gmail.com>
2012-11-23 14:12:12 -0700

    put root into the initializer.

--- spec/dummy/config/initializers/docomo.rb
+++ spec/dummy/config/initializers/docomo.rb
@@ -1,5 +1,6 @@
 Docomo.setup do |config|
 
+  config.root = Rails.root
   config.title = "Utensils Documentation"
   config.manifest_matcher = ["app/assets/utensils/**/*.md"]
   config.use_pygments = false
